3e082b81faf7aeed3c5612cf2b0dbce4a6ee5031
Rodrigo Lourenço <rodrigofplourenco2002@gmail.com>
2023-02-08 18:50:07 +0000

    git-it/repository.js

---
+++ git-it/repository.js
@@ -0,0 +1,4 @@
+// $ mkdir <FOLDERNAME> - Make a new folder (aka make directory)
+// $ cd <FOLDERNAME> - Navigate into an existing folder (aka change directory)
+// $ ls - List the items in a folder
+// $ git init - Turn git on for a folder
